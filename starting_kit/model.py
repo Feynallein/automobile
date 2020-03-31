c67e63401ec917befff214f03583e74f43275f8f
Martin Berthier <58220528+Feynallein@users.noreply.github.com>
2020-03-31 17:02:49 +0200

    Add files via upload

--- starting_kit/model.py
+++ starting_kit/model.py
@@ -3,37 +3,89 @@ from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn import linear_model
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import ExtraTreesClassifier
+
+class preprocess:
+	def __init__(self, n_components=10):
+		self.n_components = n_components
+		self.pca = None
+
+	#def outlierDetection(self, data):
+	    #clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+	    #outlier = clf.fit_predict(data)
+	    #countInlier = 0
+	    #for i in outlier:
+	    #    if i == 1:
+	    #        countInlier = countInlier + 1
+	    #realData = np.ndarray(shape=(countInlier,60))
+	    #count = 0
+	    #for i in range(len(outlier)):
+	    #    if outlier[i] == 1:
+	    #        realData[count] = data[i]
+	    #        count = count + 1
+	    #return realData
+
+	#def featuresSelection(slef, data, dataTarget, optionalEstimator, max_depth_tree, min_sample_leaf_tree):
+	 	#clf = ExtraTreesClassifier(n_estimators=optionalEstimator, max_depth=max_depth_tree, min_samples_leaf=min_sample_leaf_tree) 
+	  	#clf = clf.fit(data, dataTarget)
+	    #model = SelectFromModel(clf, threshold="mean", prefit=True)
+	    #realData = model.transform(data)
+	    #return realData
+	
+	def dimensionReduction_fit(self, data):
+		self.pca = PCA(n_components=self.n_components)
+		realData = np.ndarray(shape=(data.shape[0], self.n_components))
+		self.pca.fit(data)
+
+	def dimensionReduction_transform(self, data):
+		realData = self.pca.transform(data)
+		return realData
+
+
+	def fit(self, data):
+		#entrainement du processing
+		self.dimensionReduction_fit(data)
+
+		#c'est ici qu'il faut rajouter l'appel a d'autre preprocess (si besoin d'entrainement)
+		#sinon directement dans transform
+
+	def transform(self, data):
+		#transformation des données a l'aide d'un preprocessing deja entrainé
+		data_transformed = self.dimensionReduction_transform(data)
+
+		#ici on rappel pour d'autre preprocess si pas besoin d'entrainement
+
+		return data_transformed
+
+
+	def fit_transform(self, data):
+		self.fit(data)
+		data_transformed = self.transform(data)
+		return data_transformed
+
 
 class model (BaseEstimator):
     def __init__(self):
         self.num_train_samples=0
-        self.num_feat=1
-        self.num_labels=1
         self.is_trained=False
+        self.preprocess = preprocess(n_components=10)
         self.mod = linear_model.Ridge()
     
     def fit(self, X, y):
-        self.num_train_samples = X.shape[0]
-        if X.ndim>1: self.num_feat = X.shape[1]
-        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
-        num_train_samples = y.shape[0]
-        if y.ndim>1: self.num_labels = y.shape[1]
-        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
-        if (self.num_train_samples != num_train_samples):
-            print("ARRGH: number of samples in X and y do not match!")
+
+    	#preprocessing
+        X = self.preprocess.fit_transform(X)
+
+        #entrainement
         self.mod.fit(X,y)
         self.is_trained = True
 
     def predict(self, X):
-        num_test_samples = X.shape[0]
-        if X.ndim>1: num_feat = X.shape[1]
-        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
-        if (self.num_feat != num_feat):
-            print("ARRGH: number of features in X does not match training data!")
-        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        y = np.zeros([num_test_samples, self.num_labels])
-        # If you uncomment the next line, you get pretty good results for the Iris data :-)
-        # y = self.mod.predict(X)
+        X = self.preprocess.transform(X) #preprocessing
+        y = self.mod.predict(X) #prediction
         return y
 
     def save(self, outname='model'):
